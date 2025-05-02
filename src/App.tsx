2aa06612d5e6bdc1938cf597a857b3e03cdd487b
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-05-02 16:05:19 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from '@pages/Home';
+function App() {
+  return (
+    <>
+      <div className='px-4 md:px-8 lg:px-16 2xl:px-64'>
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
