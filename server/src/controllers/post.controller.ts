52a5e44da8e4f7cdfb91ea0862dd54c41be9fc5f
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-05-14 22:41:18 +0700

    fet: protect router by using auth clerk

--- server/src/controllers/post.controller.ts
+++ server/src/controllers/post.controller.ts
@@ -1,4 +1,5 @@
 import Post from '@/models/post.model';
+import userModel from '@/models/user.model';
 import { Request, Response } from 'express';
 
 export const getPosts = async (req: Request, res: Response) => {
@@ -12,13 +13,40 @@ export const getPost = async (req: Request, res: Response) => {
 };
 
 export const createPost = async (req: Request, res: Response) => {
-  const post = new Post(req.body);
+  const clerkUserId = req.auth?.userId;
+
+  if (!clerkUserId) {
+    res.status(401).json('Not authenticaed');
+  }
+
+  const user = await userModel.findOne({ clerkUserId });
+
+  if (!user) {
+    res.status(401).json('User not found');
+  }
+
+  const post = new Post({ user: user?._id, ...req.body });
   await post.save();
   res.status(200).json(post);
 };
 
 export const deletePost = async (req: Request, res: Response) => {
-  const post = await Post.findOneAndDelete({ _id: req.params.id });
+  const clerkUserId = req.auth?.userId;
+
+  if (!clerkUserId) {
+    res.status(401).json('Not authenticaed');
+  }
+
+  const user = await userModel.findOne({ clerkUserId });
+
+  if (!user) {
+    res.status(401).json('User not found');
+  }
+
+  const post = await Post.findOneAndDelete({
+    _id: req.params.id,
+    user: user?._id,
+  });
   if (!post) {
     res.status(404).json({ error: 'Post not found' });
   }
