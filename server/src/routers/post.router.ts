a3fd540aa2415fe06967d4b89d956c1271197e26
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-05-15 21:29:40 +0700

    bug: req.auth.userId is undefined ; fix: apply middlerware for post router

--- server/src/routers/post.router.ts
+++ server/src/routers/post.router.ts
@@ -4,13 +4,14 @@ import {
   getPost,
   getPosts,
 } from '@/controllers/post.controller';
+import { requireAuth } from '@clerk/express';
 import { Router } from 'express';
 
 const expressRouter = Router();
 
 expressRouter.get('/', getPosts);
 expressRouter.get('/:slug', getPost);
-expressRouter.post('/', createPost);
-expressRouter.delete('/:id', deletePost);
+expressRouter.post('/', requireAuth(), createPost);
+expressRouter.delete('/:id', requireAuth(), deletePost);
 
 export default expressRouter;
