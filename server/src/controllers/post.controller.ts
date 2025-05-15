68c2e37a54a9711bf58300ce8c6742235bece361
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-05-15 22:38:24 +0700

    refactor: enhance post controller by adding NextFunction for error handling

--- server/src/controllers/post.controller.ts
+++ server/src/controllers/post.controller.ts
@@ -1,6 +1,6 @@
 import Post from '@/models/post.model';
 import userModel from '@/models/user.model';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export const getPosts = async (req: Request, res: Response) => {
   const posts = await Post.find();
@@ -12,43 +12,64 @@ export const getPost = async (req: Request, res: Response) => {
   res.status(200).json(post);
 };
 
-export const createPost = async (req: Request, res: Response) => {
-  const clerkUserId = req.auth?.userId;
+export const createPost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const clerkUserId = req.auth?.userId;
+    console.log('Headers:', req.headers);
+    console.log('clerkUserId:', clerkUserId);
 
-  if (!clerkUserId) {
-    res.status(401).json('Not authenticaed');
+    if (!clerkUserId) {
+      res.status(401).json({ message: 'Not authenticated' });
+      return;
+    }
+
+    const user = await userModel.findOne({ clerkUserId });
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    const post = new Post({ user: user._id, ...req.body });
+    await post.save();
+    res.status(201).json(post);
+  } catch (error) {
+    next(error);
   }
-
-  const user = await userModel.findOne({ clerkUserId });
-
-  if (!user) {
-    res.status(401).json('User not found');
-  }
-
-  const post = new Post({ user: user?._id, ...req.body });
-  await post.save();
-  res.status(200).json(post);
 };
 
-export const deletePost = async (req: Request, res: Response) => {
-  const clerkUserId = req.auth?.userId;
+export const deletePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const clerkUserId = req.auth?.userId;
+    console.log('Headers:', req.headers.authorization);
 
-  if (!clerkUserId) {
-    res.status(401).json('Not authenticaed');
+    if (!clerkUserId) {
+      return res.status(401).json({ error: 'Not authenticated' });
+    }
+
+    const user = await userModel.findOne({ clerkUserId });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    const post = await Post.findOneAndDelete({
+      _id: req.params.id,
+      user: user?._id,
+    });
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (error) {
+    next(error);
   }
-
-  const user = await userModel.findOne({ clerkUserId });
-
-  if (!user) {
-    res.status(401).json('User not found');
-  }
-
-  const post = await Post.findOneAndDelete({
-    _id: req.params.id,
-    user: user?._id,
-  });
-  if (!post) {
-    res.status(404).json({ error: 'Post not found' });
-  }
-  res.status(200).json({ message: 'Post deleted successfully' });
 };
