6c44cef4518a7990d145f5325338a758780a52bd
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-05-20 16:48:06 +0700

    fet: add patch method

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,8 @@ dotenv.config();
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true,
 };
 
 const app = express();
